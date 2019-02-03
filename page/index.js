138038193729d6e07066d35c0e0766d3361bafa6
Mr.Hope <zhangbowang1998@gmail.com>
2019-02-03 11:14:47 +0800

    最简单的小程序

---
+++ page/index.js
@@ -0,0 +1,11 @@
+Page({
+  onLoad() {
+    console.log("这里是index页的onLoad")
+  },
+  onShow() {
+    console.log("这里是index页的onShow")
+  },
+  onReady() {
+    console.log("这里是index页的onReady")
+  }
+})
