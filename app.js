4ce6d54b81d9c6039240100d87509151168b310d
Mr.Hope <zhangbowang1998@gmail.com>
2019-02-03 12:27:04 +0800

    tab页配置

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 App({
   onLaunch() {
     console.log("这里是APP的onLaunch,APP开始启动")
-    console.log("请注意各生命周期的触发顺序")
+    console.log("请重复切换tab页时注意各生命周期的触发顺序")
   },
   onShow() {
     console.log("这里是APP的onShow")
---
+++ page/function.js
@@ -0,0 +1,14 @@
+Page({
+  onLoad() {
+    console.log("这里是function页的onLoad")
+  },
+  onShow() {
+    console.log("这里是function页的onShow")
+  },
+  onHide() {
+    console.log("这里是function页的onHide")
+  },
+  onReady() {
+    console.log("这里是function页的onReady")
+  }
+})
