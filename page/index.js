6fd74ec60d7b5aa91b0801330477091040bbe5f2
Mr.Hope <zhangbowang1998@gmail.com>
2019-10-30 21:12:06 +0800

    Update

--- page/index.js
+++ page/index.js
@@ -1,11 +1,15 @@
+// 每一个页面都应该使用 `Page()` 函数注册
 Page({
+  // onLoad 会在页面加载时触发
   onLoad() {
-    console.log("这里是index页的onLoad")
+    console.log("这里是index页的onLoad");
   },
+  // onShow 会在页面第一次展示时触发
   onShow() {
-    console.log("这里是index页的onShow")
+    console.log("这里是index页的onShow");
   },
+  // onReady 会在页面首次渲染完成时触发
   onReady() {
-    console.log("这里是index页的onReady")
+    console.log("这里是index页的onReady");
   }
-})
+});
